911579b96450221f9b0a85567606a3425df6e80c
Abhay Kumar Mehtre <abhay.mehtre@gmail.com>
2021-07-01 09:45:29 +0530

    New files

---
+++ scripts/fetch_kaggle_dataset.sh
@@ -0,0 +1,2 @@
+kaggle competitions download -c house-prices-advanced-regression-techniques -f train.csv -p packages/regression_model/regression_model/datasets/
+kaggle competitions download -c house-prices-advanced-regression-techniques -f test.csv -p packages/regression_model/regression_model/datasets/
